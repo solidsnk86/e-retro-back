5c501d45fef3383fb911da0e1645f40a0127aa76
solidsnk86 <calcagni.gabriel86@gmail.com>
2025-10-21 20:23:10 -0300

    updated method

--- src/controllers/email.controller.js
+++ src/controllers/email.controller.js
@@ -2,14 +2,16 @@ import nodemailer from "nodemailer";
 import { emailTemplate } from "../templates/emailTemplate.js";
 
 export class EmailController {
-    constructor(userName, userEmail) {
-        this.userName = userName
-        this.userEmail = userEmail
-    }
+  constructor(userName, userEmail) {
+    this.userName = userName;
+    this.userEmail = userEmail;
+  }
+
   sendMail = async () => {
     try {
-      if (!this.userEmail || !this.userEmail)
+      if (!this.userName || !this.userEmail) {
         throw new Error("Faltan los parámetros del usuario");
+      }
 
       const transporter = nodemailer.createTransport({
         service: "gmail",
@@ -18,19 +20,23 @@ export class EmailController {
           pass: process.env.EMAIL_PASS,
         },
       });
+
       const html = emailTemplate({
         userName: this.userName,
         url: "https://task-app-double-commit.vercel.app/login",
       });
 
-      transporter.sendMail({
-        from: process.env.EMAIL_USER,
+      const info = await transporter.sendMail({
+        from: `"doubleCommit taskApp" <${process.env.EMAIL_USER}>`,
         to: this.userEmail,
         subject: "Bienvenido a doubleCommit taskApp 🚀",
         html,
       });
+
+      return info;
     } catch (error) {
-      console.error(error.message);
+      console.error("Error al enviar el correo:", error.message);
+      throw error;
     }
   };
 }
