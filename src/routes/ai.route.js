78add2cd0a747c691b0378a8070cb26fece4bfde
solidsnk86 <calcagni.gabriel86@gmail.com>
2025-10-22 06:10:58 -0300

    remove

--- src/routes/ai.route.js
+++ src/routes/ai.route.js
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import { AiController } from "../controllers/ai.controller.js";
-import { isAuth } from "../middleware/isAuth.js"
 
 export const aiRoute = Router()
 
-aiRoute.get("/ai", isAuth, AiController.init)
+aiRoute.get("/ai", AiController.init)
