aa6ddd7c2d77c8d9092884bb57bb9c95829c60f2
solidsnk86 <calcagni.gabriel86@gmail.com>
2025-10-23 12:23:26 -0300

    updated query

--- src/controllers/constants.js
+++ src/controllers/constants.js
@@ -6,7 +6,7 @@ const CREATE_TASK =
 const UPDATE_TASK =
   "UPDATE tareas SET titulo = $2, descripcion = $3, actualizado = $4, actualizado_el = $5 WHERE tarea_id = $1 RETURNING *;";
 const DELETE_TASK = "DELETE FROM tareas WHERE tarea_id = $1 AND user_id = $2 RETURNING *;";
-const SET_TASK_DONE = "UPDATE tareas SET hecha = $2 WHERE tarea_id = $3 AND user_id = $1;"
+const SET_TASK_DONE = "UPDATE tareas SET hecha = $2 WHERE tarea_id = $3 AND user_id = $1 RETURNING hecha;"
 const GET_ALL_USERS = "SELECT * FROM pern_user;";
 const CREATE_USER =
   "INSERT INTO pern_user (user_name, user_email, user_password, user_avatar) VALUES($1, $2, $3, $4) RETURNING *;";
