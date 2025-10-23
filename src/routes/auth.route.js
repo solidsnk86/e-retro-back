406908c4996fc88ca0651803c7a7ab6499df3ff0
solidsnk86 <calcagni.gabriel86@gmail.com>
2025-10-23 01:54:23 -0300

    updated dependency

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import { UserController } from "../controllers/auth.controller.js";
 import { serverNeonDB } from "../../neon/neonDbConfig.js";
-import { pgLocalDB } from "../dbConfig.js";
+// import { pgLocalDB } from "../dbConfig.js";
 import { isAuth } from "../middleware/isAuth.js";
 import { CommentsController } from "../controllers/comments.controller.js";
 
 export const authRouter = Router();
 // This allows that if tomorrow you change the DB, you only pass another instance without touching the internal logic of the controller.
-//const userController = new UserController({ authDb: serverNeonDB }); // <- We inject the NeonDB server-less dependency
-const userController = new UserController({ authDb: pgLocalDB }) //<- if we want to use the pgAdmin DB
+const userController = new UserController({ authDb: serverNeonDB }); // <- We inject the NeonDB server-less dependency
+//const userController = new UserController({ authDb: pgLocalDB }) //<- if we want to use the pgAdmin DB
 const commentsControlller = new CommentsController({ authDB: serverNeonDB })
 
 authRouter.get("/users", userController.getAllUsers)
