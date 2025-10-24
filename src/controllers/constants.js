1178e9a9575f320a3cafc326dd51f8598779f861
solidsnk86 <calcagni.gabriel86@gmail.com>
2025-10-24 15:30:35 -0300

    changed querys

--- src/controllers/constants.js
+++ src/controllers/constants.js
@@ -1,12 +1,14 @@
-const GET_ALL_TASKS = "SELECT * FROM tareas WHERE user_id = $1;";
-const GET_TASK_BY_ID = "SELECT * FROM tareas WHERE tarea_id = $1;";
-const GET_TASK_BY_TITLE = "SELECT * FROM tareas WHERE titulo = $1 AND user_id = $2;"
+// TASKS
+const GET_ALL_TASKS = "SELECT * FROM tasks WHERE user_id = $1;";
+const GET_TASK_BY_ID = "SELECT * FROM tasks WHERE task_id = $1;";
+const GET_TASK_BY_TITLE = "SELECT * FROM tasks WHERE title = $1 AND user_id = $2;"
 const CREATE_TASK =
-  "INSERT INTO tareas(titulo, descripcion, user_id) VALUES($1, $2, $3) RETURNING *;";
+  "INSERT INTO tasks(title, description, user_id) VALUES($1, $2, $3) RETURNING *;";
 const UPDATE_TASK =
-  "UPDATE tareas SET titulo = $2, descripcion = $3, actualizado = $4, actualizado_el = $5 WHERE tarea_id = $1 RETURNING *;";
-const DELETE_TASK = "DELETE FROM tareas WHERE tarea_id = $1 AND user_id = $2 RETURNING *;";
-const SET_TASK_DONE = "UPDATE tareas SET hecha = $2 WHERE tarea_id = $3 AND user_id = $1 RETURNING hecha;"
+  "UPDATE tasks SET title = $2, description = $3, task_updated = $4, updated_at = $5 WHERE task_id = $1 RETURNING *;";
+const DELETE_TASK = "DELETE FROM tasks WHERE task_id = $1 AND user_id = $2 RETURNING *;";
+const SET_TASK_DONE = "UPDATE tasks SET task_done = $2 WHERE task_id = $3 AND user_id = $1 RETURNING task_done;"
+// USER
 const GET_ALL_USERS = "SELECT * FROM pern_user;";
 const CREATE_USER =
   "INSERT INTO pern_user (user_name, user_email, user_password, user_avatar) VALUES($1, $2, $3, $4) RETURNING *;";
@@ -17,6 +19,7 @@ const UPDATE_USER =
   "UPDATE pern_user SET user_name = $2, user_email = $3, user_password = $4, user_update = $5, updated_at = $6, user_avatar = $7 WHERE user_id = $1 RETURNING *;";
 const UPDATE_USER_PASSWORD = "UPDATE pern_user SET user_password = $2, user_update = $3 WHERE user_id = $1;";
 const DELETE_USER = "DELETE FROM pern_user WHERE user_id = $1 RETURNING *;";
+// COMMENTS
 const GET_ALL_COMMETS = "SELECT * FROM comments;"
 const CREATE_COMMENT = "INSERT INTO comments(title, message) VALUES($1, $2) RETURNING *;"
 
